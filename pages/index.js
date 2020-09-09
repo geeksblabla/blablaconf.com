a95d3c68cca569739c44297f8901a0061f94911a
Soufian El <vegetaf09@localhost.localdomain>
2020-09-08 22:29:12 -0400

    background hpme part 1

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-import Head from 'next/head'
-// import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 import styled, { createGlobalStyle } from "styled-components";
-import TopLeftIcon from '../svg/top_left_icon';
-import TopRightIcon from '../svg/top_right_icon';
-import BottomLeftIcon from '../svg/bottom_left_icon';
-import BottomRightIcon from '../svg/bottom_right_icon';
-
+import TopLeftIcon from "../svg/top_left_icon";
+import TopRightIcon from "../svg/top_right_icon";
+import BottomLeftIcon from "../svg/bottom_left_icon";
+import BottomRightIcon from "../svg/bottom_right_icon";
 
 const GlobalStyle = createGlobalStyle`
   html,
@@ -15,38 +14,45 @@ const GlobalStyle = createGlobalStyle`
   div#__next > div,
   div#__next > div > div {
     height: 100%;
+    background: linear-gradient(300.87deg, #220C79 3.7%, #4A37AB 100%);
+    min-height: 100%;
   }
 `;
 
-const Home = ({ className }) => {
+const Home = () => {
   return (
-    <div className={className}>
+    <div>
       <GlobalStyle />
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <TopLeftIcon className="top_left_icon" />
-        <TopRightIcon className="top_right_icon" />
-        <BottomLeftIcon className="bottom_left_icon" />
-        <BottomRightIcon className="bottom_right_icon" />
+      <main className={styles.main}>
+        <div className={styles.top_icons}>
+          <div className={styles.top_left_icon}>
+            <TopLeftIcon />
+          </div>
+          <div className={styles.top_right_icon}>
+            <TopRightIcon />
+          </div>
+      
+        </div>
+        <div className={styles.footer}>
+        <div className={styles.bottom_icons}>
+          <div className={styles.bottom_left_icon}>
+            <BottomLeftIcon />
+          </div>
+          <div className={styles.bottom_right_icon}>
+            <BottomRightIcon />
+          </div>
+        </div>
+
+          <div className={styles.footer_text}> Copyright © 2020 BlablaConf. All rights reserved.</div>
+        </div>
       </main>
- 
     </div>
-  )
-}
+  );
+};
 
-const StyledHome = styled(Home)`
-  background: linear-gradient(300.87deg, #220C79 3.7%, #4A37AB 100%);
-  min-height: 100%;
-
-  .top_left_icon{
-    margin-top: 78px;
-    margin-left: 186px;
-  }
-
-`
-
-export default StyledHome;
+export default Home;
