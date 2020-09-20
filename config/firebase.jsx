4f89f4d6bf280fe581c64d4f82cba4d24dda6ecd
soufian <sfoukahi@gmail.co>
2020-09-20 13:16:15 -0400

    refactoring

--- config/firebase.jsx
+++ config/firebase.jsx
@@ -1,13 +1,19 @@
 import firebase from "firebase";
 import "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_DATA_BASE_URL,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-};
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+//   databaseURL: process.env.NEXT_PUBLIC_DATA_BASE_URL,
+//   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+// };
  
+const firebaseConfig = {
+  apiKey: "AIzaSyDugS9zg_Y79x6V6NHs_C5kzHm6xZQJlWg",
+  authDomain: "lablaconf-b56b8.firebaseapp.com",
+  databaseURL: "https://blablaconf-b56b8.firebaseio.com",
+  projectId: "blablaconf-b56b8",
+}; 
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
