3888bfe763fbe5842530c4b82c054edf145dbf54
Soufian El <vegetaf09@localhost.localdomain>
2020-09-25 22:11:43 -0400

    register bottom

--- components/hero.jsx
+++ components/hero.jsx
@@ -3,16 +3,15 @@ import styles from "../styles/Home.module.css";
 import Registration from "./registration";
 
 const Hero = () => {
- 
   return (
-       <div className={styles.mainDiv}>
-        <div className={styles.title}>
-          5 Days and 5 Tracks covering hottest Technology Trends in Darija
-        </div>
-        <div className={styles.subtitle}>19-23 October | Online</div>
-        <Registration />
+    <div className={styles.mainDiv}>
+      <div className={styles.title}>
+        5 Days and 5 Tracks covering hottest Technology Trends in Darija
       </div>
-   );
+      <div className={styles.subtitle}>19-23 October | Online</div>
+      <Registration />
+    </div>
+  );
 };
 
 export default Hero;
--- pages/index.js
+++ pages/index.js
@@ -30,13 +30,18 @@ const Index = () => {
           type="text/css"
           href="//fonts.googleapis.com/css?family=Raleway"
         />
+        <link
+          href="https://fonts.googleapis.com/css?family=Inter"
+          rel="stylesheet"
+        />
+
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
       </Head>
       <div className={styles.header}>
-      <div className={styles.logoContaier}>
+        <div className={styles.logoContaier}>
           <div className={styles.logo}>
             <Link href="/">
               <a>
@@ -47,21 +52,33 @@ const Index = () => {
           <span className={styles.powredBy}>powered by #DevC_Morocco</span>
         </div>
         <Hero />
-
       </div>
-      {/* <div className={styles.header}>
 
-     
-
-
-        
+     <div className={styles.container}>
+     <div className={styles.about}>
+        <div className={styles.aboutTitle}>About Blablaconf</div>
+        <div className={styles.aboutDescription}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa dui sed
+          eu elementum vulputate viverra sapien eget. Aliquet elementum ultrices
+          dolor facilisi arcu. Molestie tortor, nec suspendisse faucibus sodales
+          nisi, scelerisque mauris. Sit duis dui sed risus, etiam etiam pulvinar
+          mi.
+        </div>
       </div>
+     </div>
+     <div className={styles.registerBottom}>
+
+     </div>
+
 
       <div className={styles.footer} style={{ flexDirection: "column" }}>
         <div className={styles.footer_text}>
           Copyright © 2020 BlablaConf. All rights reserved.
         </div>
-      </div> */}
+      </div>
+      {/* 
+
+   */}
     </div>
   );
 };
