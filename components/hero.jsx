540b4268218318d7287577c4676e54a93bc1fd72
Soufian El <vegetaf09@localhost.localdomain>
2020-09-25 21:01:31 -0400

    update the header

--- components/hero.jsx
+++ components/hero.jsx
@@ -5,16 +5,14 @@ import Registration from "./registration";
 const Hero = () => {
  
   return (
-    <div>
-      <div className={styles.mainDiv}>
+       <div className={styles.mainDiv}>
         <div className={styles.title}>
           5 Days and 5 Tracks covering hottest Technology Trends in Darija
         </div>
         <div className={styles.subtitle}>19-23 October | Online</div>
         <Registration />
       </div>
-    </div>
-  );
+   );
 };
 
 export default Hero;
--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,67 @@ import Layout from "../components/layout";
 import { createGlobalStyle } from "styled-components";
 import Hero from "../components/hero";
 import ComingSoon from "../components/coming_soon";
-  
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import TopLeftIcon from "../svg/top_left_icon";
+import TopRightIcon from "../svg/top_right_icon";
+import BottomLeftIcon from "../svg/bottom_left_icon";
+import BottomRightIcon from "../svg/bottom_right_icon";
+import Logo from "../svg/logo";
+import Link from "next/link";
 //background: linear-gradient(300.87deg, #220C79 3.7%, #4A37AB 100%) no-repeat;
-  
+
 const Index = () => {
   return (
-     <Layout title="BlaBlaConf Registration">
-        <Hero /> 
-     </Layout>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+        minWidth: "100%",
+      }}
+    >
+      <Head>
+        <title>BlablaConf</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="//fonts.googleapis.com/css?family=Raleway"
+        />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+      </Head>
+      <div className={styles.header}>
+      <div className={styles.logoContaier}>
+          <div className={styles.logo}>
+            <Link href="/">
+              <a>
+                <Logo />
+              </a>
+            </Link>
+          </div>
+          <span className={styles.powredBy}>powered by #DevC_Morocco</span>
+        </div>
+        <Hero />
+
+      </div>
+      {/* <div className={styles.header}>
+
+     
+
+
+        
+      </div>
+
+      <div className={styles.footer} style={{ flexDirection: "column" }}>
+        <div className={styles.footer_text}>
+          Copyright © 2020 BlablaConf. All rights reserved.
+        </div>
+      </div> */}
+    </div>
   );
 };
 
