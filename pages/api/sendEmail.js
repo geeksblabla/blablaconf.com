818242774a725b653fbcd6a93fcac96d86b43728
Soufian El <vegetaf09@localhost.localdomain>
2020-09-25 22:18:49 -0400

    refactoring

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -5,7 +5,6 @@ const mailgun = require("mailgun-js");
 export default (req, res) => {
   if (req.method === "POST") {
     const email = req.body.email;
-    // cb3791c4-804564cc	
     const DOMAIN = "mail.blablaconf.com";
     const mg = mailgun({
       apiKey: process.env.NEXT_PUBLIC_MAIL_GUN_API_KEY,
@@ -20,20 +19,7 @@ export default (req, res) => {
     };
     mg.messages().send(data, function (error, body) {
       console.log(body);
-    });
-
-    // const email = req.body.email;
-
-    // sgMail.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY);
-    // const msg = {
-    //   to: email,
-    //   from: "regitration@blablaconf.com",
-    //   subject: "BlaBlaConf Registration",
-    //   text: "Welcome to BlaBlaConf",
-    //   html: "<strong>Welcome to BlaBlaConf</strong>",
-    // };
-
-    // sgMail.send(msg);
+    }); 
     res.json({ result: "ok" });
   }
 };
