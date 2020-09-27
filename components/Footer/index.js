d51d2b176dc06e856a07a7bf5978a3cb4539c4e4
Youssouf El Azizi <youssoufelazizi@gmail.com>
2020-09-27 18:18:22 +0100

    add footer section

---
+++ components/Footer/index.js
@@ -0,0 +1,12 @@
+import styles from "./index.module.css";
+
+export const Footer = () => (
+  <div className={styles.footer}>
+    <div className={styles.container}>
+      <p> This Website made with ❤️ By DevC Team using, sourced on GitHub.</p>
+      <p>
+        Copyright © {new Date().getFullYear()} Blabla Conf. All rights reserved.
+      </p>
+    </div>
+  </div>
+);
