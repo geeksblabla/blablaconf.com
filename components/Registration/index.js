1d60fa0013b99a4c7e4c5e05fef78620fc286a93
Youssouf El Azizi <youssoufelazizi@gmail.com>
2020-09-27 18:18:09 +0100

    last section

---
+++ components/Registration/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Header } from "../Header";
+import styles from "./index.module.css";
+import Form from "./registration";
+
+export const Registration = () => {
+  return (
+    <div className={styles.registration}>
+      <div className={styles.container}>
+        <div className={styles.main}>
+          <div className={styles.form}>
+            <h1 className={styles.title}>
+              What are you waiting for? Claim your spot now!
+            </h1>
+            <Form />
+          </div>
+          <img src="/tickets.png" className={styles.tickets} />
+        </div>
+      </div>
+    </div>
+  );
+};
