4a89d6753e98a9262d5a517611f0bf0fdb6fadca
Youssouf El Azizi <youssoufelazizi@gmail.com>
2020-09-30 01:32:19 +0100

    fix layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,13 @@
-import { Container } from "next/app";
 import "../styles/globals.css";
 import { DefaultSeo } from "next-seo";
 import SEO from "../next-seo.config.js";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Container>
+    <>
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
-    </Container>
+    </>
   );
 }
 
