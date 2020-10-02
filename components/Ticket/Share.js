2365f7cd623740ec5c674efc649872abf46fae5c
Youssouf El Azizi <youssoufelazizi@gmail.com>
2020-10-02 19:11:07 +0100

    update layout

--- components/Ticket/Share.js
+++ components/Ticket/Share.js
@@ -2,37 +2,72 @@ import {
   FacebookShareButton,
   LinkedinShareButton,
   TwitterShareButton,
-  WhatsappShareButton,
-  FacebookIcon,
-  TwitterIcon,
-  LinkedinIcon,
-  WhatsappIcon,
-} from "react-share"
+} from "react-share";
+import styles from "./index.module.css";
 
-export default ({ shareUrl, title: t }) => {
-  const title = t + "#GeeksBalala #DevC_Casa"
+import { Facebook, Twitter, Linkedin, Link } from "./icons";
+import { useState, useEffect } from "react";
+
+const copyToClipboard = (str) => {
+  const el = document.createElement("textarea");
+  el.value = str;
+  el.setAttribute("readonly", "");
+  el.style.position = "absolute";
+  el.style.left = "-9999px";
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand("copy");
+  document.body.removeChild(el);
+};
+
+export default ({ shareUrl }) => {
+  const [copied, setCopied] = useState(false);
+  useEffect(() => {
+    if (copied) setTimeout(() => setCopied(false), 2000);
+  }, [copied]);
+  const title =
+    "👋 I am attending #BlaBlaConf Grab your ticket too 👇👇  #GeeksBalBla #DevC_Morocco";
 
   return (
-    <div className="episode-actions">
-      <div className="share">
-        {/* <p> Share Episode On : </p> */}
-        <FacebookShareButton url={shareUrl} quote={title}>
-          <FacebookIcon size={32} round />
+    <div>
+      <p className={styles.description}>
+        You can share your ticket using the link{" "}
+      </p>
+      <button
+        className={styles.copy}
+        onClick={() => {
+          setCopied(true);
+          copyToClipboard(shareUrl);
+        }}
+      >
+        <Link className={styles.link} />{" "}
+        {copied ? "Link Copied" : "Copy Ticket URL"}
+      </button>
+      <div className={styles.icons}>
+        <p className={styles.description}>Or share directly on </p>
+        <FacebookShareButton
+          url={shareUrl}
+          quote={title}
+          className={styles.icon}
+        >
+          <Facebook />
         </FacebookShareButton>
-        <TwitterShareButton url={shareUrl} title={title}>
-          <TwitterIcon size={32} round />
+        <TwitterShareButton
+          url={shareUrl}
+          title={title}
+          className={styles.icon}
+        >
+          <Twitter />
         </TwitterShareButton>
         <LinkedinShareButton
+          className={styles.icon}
           url={shareUrl}
           windowWidth={750}
           windowHeight={600}
         >
-          <LinkedinIcon size={32} round />
+          <Linkedin />
         </LinkedinShareButton>
-        <WhatsappShareButton url={shareUrl} title={title} separator=":: ">
-          <WhatsappIcon size={32} round />
-        </WhatsappShareButton>
       </div>
     </div>
-  )
-}
+  );
+};
--- components/Ticket/index.js
+++ components/Ticket/index.js
@@ -2,56 +2,51 @@ import { useState } from "react";
 import { Header } from "../Header";
 import styles from "./index.module.css";
 import { Container } from "../Container";
-import { getTicketImg } from "./utils";
+import { getTicketImg } from "../utils";
 import Popup from "reactjs-popup";
 import Girl from "./Girl";
 import Boy from "./Boy";
-// const Ser
 import { auth, firebase, firestore } from "../../config/firebase";
 import Share from "./Share";
-
-// const ServerLessPopup = typeof window !== undefined ? <></> : <></>;
+import { Github } from "./icons";
 
 export const Ticket = () => {
   const [user, setUser] = useState(undefined);
   const [isBoy, setIsBoy] = useState(undefined);
 
   const host = process.env.NEXT_PUBLIC_HOST;
-  const shareUrl =  user ? "https://" + host + "/myticket/" + user.username : "";
-
- const copyLink = () => {
-  const el = document.createElement("textarea");
-  el.value = "https://" + host + "/myticket/" + user.username;
-  document.body.appendChild(el);
-  el.select();
-  document.execCommand("copy");
-  document.body.removeChild(el);
-  console.log(el.value);
-};
+  const shareUrl = user ? "https://" + host + "/myticket/" + user.username : "";
 
   return (
     <div className={styles.hero}>
       <Container>
-        <Header withLinks />
+        <Header />
         <div className={styles.main}>
           <div className={styles.content}>
             <h1 className={styles.title}>
               🎉🎉🎉 <br />
               Congratulation your are in!
             </h1>
-            <p className={styles.description}>
-              {!!user && "You can share your ticket using the link"}
-            </p>
-            {!user ?
-              <div suppressHydrationWarning={true}>
-                {process.browser && <SelectGenderModal isBoy={isBoy} setIsBoy={setIsBoy} user={user} setUser={setUser} />}
-              </div>
-              :
+            {!user ? (
               <>
-                <button onClick={copyLink}> coppy link</button>
-                 <Share title={"share"} shareUrl={shareUrl} />
+                <p className={styles.description}>
+                  {" "}
+                  Generate your ticket image with your Github profile
+                </p>
+                <div suppressHydrationWarning={true}>
+                  {process.browser && (
+                    <SelectGenderModal
+                      isBoy={isBoy}
+                      setIsBoy={setIsBoy}
+                      user={user}
+                      setUser={setUser}
+                    />
+                  )}
+                </div>
               </>
-            }
+            ) : (
+              <Share title={"share"} shareUrl={shareUrl} />
+            )}
           </div>
           <div className={styles.img_container}>
             <img src={getTicketImg(user)} className={styles.img} />
@@ -63,38 +58,36 @@ export const Ticket = () => {
 };
 
 const SelectGenderModal = ({ isBoy, setIsBoy, setUser }) => {
-
   const generateTicket = () => {
     const githubAuthProvider = new firebase.auth.GithubAuthProvider();
-    const uniqueNumber = new Date().valueOf();
 
-    auth
-      .signInWithPopup(githubAuthProvider)
-      .then(function (result) {
-        const user = result.user;
-        const additionalUserInfo = result.additionalUserInfo;
+    auth.signInWithPopup(githubAuthProvider).then(function (result) {
+      const user = result.user;
+      const additionalUserInfo = result.additionalUserInfo;
 
-        const tmpUser = {
-          name: user.displayName,
-          username: additionalUserInfo.username,
-          email: user.email,
-          photo: user.photoURL,
-          ticketNumber:  "000"+Math.floor(1000 + Math.random() * 9000),
-          gender: isBoy ? "boy" : "girl"
-        };
+      const tmpUser = {
+        name: user.displayName,
+        username: additionalUserInfo.username,
+        email: user.email,
+        photo: user.photoURL,
+        ticketNumber: "000" + Math.floor(1000 + Math.random() * 9000),
+        gender: isBoy ? "boy" : "girl",
+      };
 
-        firestore
-          .collection("/tickets")
-          .doc(additionalUserInfo.username)
-          .set(tmpUser);
-        setUser(tmpUser);
-      });
+      firestore
+        .collection("/tickets")
+        .doc(additionalUserInfo.username)
+        .set(tmpUser);
+      setUser(tmpUser);
+    });
   };
 
   return (
     <Popup
       trigger={
-        <button className={styles.button}> Generate with GitHub </button>
+        <button className={styles.button}>
+          <Github className={styles.github} /> Generate with GitHub{" "}
+        </button>
       }
       modal
       contentStyle={{
@@ -105,21 +98,28 @@ const SelectGenderModal = ({ isBoy, setIsBoy, setUser }) => {
     >
       <div>
         <div className={styles.modal_container}>
-          <p className={styles.modal_title}> Select your gender</p>
+          <p className={styles.modal_title}> Choose Your Style</p>
           <div className={styles.modal_options}>
             <div
               className={styles.modal_option}
               onClick={() => setIsBoy(false)}
             >
               <Girl active={isBoy !== undefined && !isBoy} />
-              <p className={styles.modal_option_text}> I’m a girl</p>
+              <p className={styles.modal_option_text}> Meriem style</p>
             </div>
             <div className={styles.modal_option} onClick={() => setIsBoy(true)}>
               <Boy active={isBoy} />
-              <p className={styles.modal_option_text}>I’m a boy</p>
+              <p className={styles.modal_option_text}> Moha Style</p>
             </div>
           </div>
-          <button disabled={isBoy === undefined} className={styles.modal_button} onClick={generateTicket}> Continue </button>
+          <button
+            disabled={isBoy === undefined}
+            className={styles.modal_button}
+            onClick={generateTicket}
+          >
+            {" "}
+            Continue{" "}
+          </button>
         </div>
       </div>
     </Popup>
