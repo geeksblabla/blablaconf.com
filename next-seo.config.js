6daf53ea7f977b1400e0f6088fc62d9d140ff7f2
Youssouf El Azizi <youssoufelazizi@gmail.com>
2020-10-03 20:24:59 +0100

    fix social graph links

--- next-seo.config.js
+++ next-seo.config.js
@@ -4,7 +4,7 @@ export default {
     "BlaBla Conf | 5 Days and 5 Tracks Covering Hottest Technology Trends in Darija",
   description:
     "By the Moroccan developer community, for the Moroccan developer community, BlaBla Conf is your one stop shop for latest and hottest technology trends, in Darija, and completely free! Join us from 20th to 24th October",
-  canonical: host,
+  canonical: `https://${host}/`,
   openGraph: {
     title:
       "BlaBla Conf | 5 Days and 5 Tracks covering hottest Technology Trends in Darija",
@@ -12,11 +12,11 @@ export default {
       "By the Moroccan developer community, for the Moroccan developer community, BlaBla Conf is your one stop shop for latest and hottest technology trends, in Darija, and completely free! Join us from 20th to 24th October",
     type: "website",
     locale: "en_IE",
-    url: host,
+    url: `https://${host}/`,
     site_name: "BlaBlaConf",
     images: [
       {
-        url: `${host}/cover.jpg`,
+        url: `https://${host}/cover.jpg`,
       },
     ],
   },
