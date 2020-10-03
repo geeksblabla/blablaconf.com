925b3106be61fdd93bae4cece7e7c2255250134b
Youssouf El Azizi <youssoufelazizi@gmail.com>
2020-10-03 15:05:03 +0100

    fix duplicated speakers  & sort

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,28 +1,33 @@
 import yaml from "js-yaml";
 
 function compare(a, b) {
-  if (a.name < b.name) {
-    return -1;
+  // Use toUpperCase() to ignore character casing
+  const bandA = a.name.toUpperCase();
+  const bandB = b.name.toUpperCase();
+
+  let comparison = 0;
+  if (bandA > bandB) {
+    comparison = 1;
+  } else if (bandA < bandB) {
+    comparison = -1;
   }
-  if (a.name > b.name) {
-    return 1;
-  }
-  return 0;
+  return comparison;
 }
 
 export async function getData() {
   const context = require.context("../../tracks/", false, /\.yml$/);
   const tracks = [];
-  const speakers = [];
+  let speakers = [];
   for (const key of context.keys()) {
     const trackName = key.slice(2);
     const content = await import(`../../tracks/${trackName}`);
     const track = yaml.safeLoad(content.default);
     tracks.push(track);
-    const trackSpeakers = track.sessions.map((s) => s.speaker).sort(compare);
-
+    const trackSpeakers = track.sessions
+      .map((s) => s.speaker)
+      .filter((sp) => !speakers.some((s) => s.name === sp.name));
     speakers.push(...trackSpeakers);
   }
 
-  return { tracks, speakers };
+  return { tracks, speakers: speakers.sort(compare) };
 }
