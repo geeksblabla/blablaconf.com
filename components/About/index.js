fc7ad84ae8f0f38ff32e667b725e7c31abfb9ed5
Abderrhim Soubai <gx.coder@gmail.com>
2020-10-04 14:02:16 +0100

    fix(about): typo in main sentence

--- components/About/index.js
+++ components/About/index.js
@@ -13,10 +13,7 @@ export const About = () => {
         <div className={styles.main}>
           <h1 className={styles.title}>Made by and for the community</h1>
           <div className={styles.description}>
-            By the Moroccan developer community, for the Moroccan developer
-            community, BlaBla Conf is your one stop shop for lattest and hottest
-            technology trends, in darija, and completely free! Join us from 20th
-            to 24th October
+            By the Moroccan developer community, for the Moroccan developer community, BlaBla Conf is your one-stop-shop for the latest and hottest technology trends, in Darija, and completely free! Join us from 20th to 24th October.
           </div>
         </div>
         <Sponsors />
