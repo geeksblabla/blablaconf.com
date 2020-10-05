978bd986d1b7364885b6c16164cea026c1607f9b
Youssouf El Azizi <youssoufelazizi@gmail.com>
2020-10-05 01:06:58 +0100

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,10 @@ import "reactjs-popup/dist/index.css";
 import { DefaultSeo } from "next-seo";
 import SEO from "../next-seo.config.js";
 import Head from "next/head";
+import Router from "next/router";
+import * as gtag from "../components/gtag";
+
+Router.events.on("routeChangeComplete", (url) => gtag.pageview(url));
 
 function MyApp({ Component, pageProps }) {
   return (
