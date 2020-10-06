3ed9370267678e37fc9ca833a1b67824c200f8c6
smakosh <ismai23l@hotmail.com>
2020-10-06 18:21:42 +0100

    feat: implement ISG

--- components/MyTicket/index.js
+++ components/MyTicket/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useRouter } from "next/router";
 import { Header } from "../Header";
 import styles from "./index.module.css";
 import { Container } from "../Container";
@@ -6,21 +7,27 @@ import { getTicketImg } from "../utils";
 import { RegistrationForm } from "../RegistrationForm/index";
 
 export const MyTicket = ({ user }) => {
-  return (
-    <div className={styles.hero}>
-      <Container>
-        <Header />
-        <div className={styles.main}>
-          <h1 className={styles.title}>
-            {user.name === null ? user.username : user.name}'s Ticket
-          </h1>
-          <div className={styles.img_container}>
-            <img src={getTicketImg(user)} className={styles.img} />
-          </div>
-          <div className={styles.date}>Join them on October 20-24 | Online</div>
-          <RegistrationForm />
-        </div>
-      </Container>
-    </div>
-  );
+	const router = useRouter();
+
+	if (router.isFallback) {
+		return <div>Loading...</div>;
+	}
+
+	return (
+		<div className={styles.hero}>
+			<Container>
+				<Header />
+				<div className={styles.main}>
+					<h1 className={styles.title}>
+						{user.name === null ? user.username : user.name}'s Ticket
+					</h1>
+					<div className={styles.img_container}>
+						<img src={getTicketImg(user)} className={styles.img} />
+					</div>
+					<div className={styles.date}>Join them on October 20-24 | Online</div>
+					<RegistrationForm />
+				</div>
+			</Container>
+		</div>
+	);
 };
--- pages/myticket/[username].js
+++ pages/myticket/[username].js
@@ -1,64 +1,64 @@
-import { firestore } from "../../config/firebase";
 import { NextSeo } from "next-seo";
+import { firestore } from "../../config/firebase";
 import { Layout, MyTicket } from "../../components";
 import { getTicketGraphImg } from "../../components/utils";
 
-const TicketPage = ({ user, seoConfig }) => {
-  return (
-    <Layout>
-      <NextSeo {...seoConfig} />
-      <MyTicket user={user} />
-    </Layout>
-  );
+const TicketPage = ({ user, seoConfig }) => (
+	<Layout>
+		<NextSeo {...seoConfig} />
+		<MyTicket user={user} />
+	</Layout>
+);
+
+export const getStaticPaths = async () => ({
+	paths: [
+		{
+			params: {
+				username: "smakosh",
+			},
+		},
+	],
+	fallback: true,
+});
+
+export const getStaticProps = async ({ params: { username } }) => {
+	let seoConfig = null;
+	let user = null;
+
+	const doc = await firestore.collection("/tickets").doc(username).get();
+
+	user = doc.data();
+	const name = user.name === null ? user.username : user.name;
+
+	seoConfig = {
+		title: name + "'s BlaBlaConf Ticket",
+		description:
+			"BlaBla Conf | 5 Days and 5 Tracks Covering Hottest Technology Trends in Darija",
+		openGraph: {
+			type: "website",
+			locale: "en_IE",
+			url: `https://${process.env.NEXT_PUBLIC_HOST}/myticket/${user.username}`,
+			title: `${name}'s BlaBlaConf Ticket`,
+			description:
+				"BlaBla Conf | 5 Days and 5 Tracks Covering Hottest Technology Trends in Darija",
+			images: [
+				{
+					url: getTicketGraphImg(user),
+				},
+			],
+			site_name: "blablaconf.com",
+			imageWidth: 1200,
+			imageHeight: 1200,
+		},
+	};
+
+	return {
+		props: {
+			user,
+			seoConfig,
+		},
+		revalidate: 2,
+	};
 };
-
-export async function getServerSideProps({ query }) {
-  const username = query.username;
-
-  let seoConfig = null;
-  let user = null;
-
-  if (username && username !== "")
-    await firestore
-      .collection("/tickets")
-      .doc(username)
-      .get()
-      .then(function (doc) {
-        user = doc.data();
-        const name = user.name === null ? user.username : user.name;
-        seoConfig = {
-          title: name + "'s BlaBlaConf Ticket",
-          description:
-            "BlaBla Conf | 5 Days and 5 Tracks Covering Hottest Technology Trends in Darija",
-          openGraph: {
-            type: "website",
-            locale: "en_IE",
-            url:
-              "https://" +
-              process.env.NEXT_PUBLIC_HOST +
-              "/myticket/" +
-              user.username,
-            title: name + "'s BlaBlaConf Ticket",
-            description:
-              "BlaBla Conf | 5 Days and 5 Tracks Covering Hottest Technology Trends in Darija",
-            images: [
-              {
-                url: getTicketGraphImg(user),
-              },
-            ],
-            site_name: "blablaconf.com",
-            imageWidth: 1200,
-            imageHeight: 1200,
-          },
-        };
-      });
-
-  return {
-    props: {
-      user,
-      seoConfig,
-    },
-  };
-}
 
 export default TicketPage;
