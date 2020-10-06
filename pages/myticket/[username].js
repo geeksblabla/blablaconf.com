5e81a403559cabbe5db0b24cd86fda06402ba24c
Ismail Ghallou <ismai23l@hotmail.com>
2020-10-06 20:51:58 +0000

    chore: fix indention

--- pages/myticket/[username].js
+++ pages/myticket/[username].js
@@ -4,61 +4,61 @@ import { Layout, MyTicket } from "../../components";
 import { getTicketGraphImg } from "../../components/utils";
 
 const TicketPage = ({ user, seoConfig }) => (
-	<Layout>
-		<NextSeo {...seoConfig} />
-		<MyTicket user={user} />
-	</Layout>
+  <Layout>
+    <NextSeo {...seoConfig} />
+    <MyTicket user={user} />
+  </Layout>
 );
 
 export const getStaticPaths = async () => ({
-	paths: [
-		{
-			params: {
-				username: "smakosh",
-			},
-		},
-	],
-	fallback: true,
+  paths: [
+    {
+      params: {
+        username: "smakosh",
+      },
+    },
+  ],
+  fallback: true,
 });
 
 export const getStaticProps = async ({ params: { username } }) => {
-	let seoConfig = null;
-	let user = null;
+  let seoConfig = null;
+  let user = null;
 
-	const doc = await firestore.collection("/tickets").doc(username).get();
+  const doc = await firestore.collection("/tickets").doc(username).get();
 
-	user = doc.data();
-	const name = user.name === null ? user.username : user.name;
+  user = doc.data();
+  const name = user.name === null ? user.username : user.name;
 
-	seoConfig = {
-		title: name + "'s BlaBlaConf Ticket",
-		description:
-			"BlaBla Conf | 5 Days and 5 Tracks Covering Hottest Technology Trends in Darija",
-		openGraph: {
-			type: "website",
-			locale: "en_IE",
-			url: `https://${process.env.NEXT_PUBLIC_HOST}/myticket/${user.username}`,
-			title: `${name}'s BlaBlaConf Ticket`,
-			description:
-				"BlaBla Conf | 5 Days and 5 Tracks Covering Hottest Technology Trends in Darija",
-			images: [
-				{
-					url: getTicketGraphImg(user),
-				},
-			],
-			site_name: "blablaconf.com",
-			imageWidth: 1200,
-			imageHeight: 1200,
-		},
-	};
+  seoConfig = {
+    title: name + "'s BlaBlaConf Ticket",
+    description:
+      "BlaBla Conf | 5 Days and 5 Tracks Covering Hottest Technology Trends in Darija",
+    openGraph: {
+      type: "website",
+      locale: "en_IE",
+      url: `https://${process.env.NEXT_PUBLIC_HOST}/myticket/${user.username}`,
+      title: `${name}'s BlaBlaConf Ticket`,
+      description:
+        "BlaBla Conf | 5 Days and 5 Tracks Covering Hottest Technology Trends in Darija",
+      images: [
+        {
+          url: getTicketGraphImg(user),
+        },
+      ],
+      site_name: "blablaconf.com",
+      imageWidth: 1200,
+      imageHeight: 1200,
+    },
+  };
 
-	return {
-		props: {
-			user,
-			seoConfig,
-		},
-		revalidate: 2,
-	};
+  return {
+    props: {
+      user,
+      seoConfig,
+    },
+    revalidate: 2,
+  };
 };
 
 export default TicketPage;
