60b34e0f1cb8a44aea6a460d2b1f33e7c1187b9c
Youssouf El Azizi <youssoufelazizi@gmail.com>
2020-10-21 14:55:36 +0100

    add social links

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -15,6 +15,39 @@ export const Hero = () => {
             <span className={styles.ma}> Darija </span>
           </h1>
           <div className={styles.date}>20-24 October | Online</div>
+          <div>
+            <p className={styles.stream}>
+              Conference will be streamed in our YouTube channel & facebook page
+              👇👇👇{" "}
+            </p>
+            <div className={styles.social}>
+              <a
+                href="http://facebook.com/geeksblabla"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="svg/facebook.svg"
+                  className={styles.social_icon}
+                  height={38}
+                  alt="Geeksblabla Facebook Page "
+                />
+              </a>
+              <a
+                href="https://www.youtube.com/channel/UCW2WV7NKU0WPyuv4YoNSqBA"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ marginRight: "10px" }}
+              >
+                <img
+                  className={styles.social_icon}
+                  src="svg/youtube.svg"
+                  height={50}
+                  alt="GeeksBlabla youtube Channel "
+                />
+              </a>
+            </div>
+          </div>
           <RegistrationForm />
         </div>
       </Container>
