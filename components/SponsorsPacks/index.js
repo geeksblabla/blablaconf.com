62cc8787d5e88f6a9665fec7f922b556ad76e848
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2021-09-03 12:57:58 +0100

    refactor sponsoring packs section

--- components/SponsorsPacks/index.js
+++ components/SponsorsPacks/index.js
@@ -11,21 +11,23 @@ export const SponsorsPacks = () => {
           <h1> Diamond </h1>
           <ul>
             <li>
-              Welcome to the Diamond sponsor post on BlaBla conf social media
-              accounts
+              Identification as Diamond sponsor and the logo will be appear the
+              first with the biggest size in all marketing digital support of
+              the event
             </li>
-            <li>Logo on BlaBla Conf homepage (Diamond sponsor section)</li>{" "}
-            <li>Logo on event promotional materials</li>{" "}
             <li>
               Guaranteed 2 conference slot (30 min) [Should be of different
               tracks]
+            </li>{" "}
+            <li>
+              Special Thanks & welcome to the Diamond sponsor in our social
+              medias
             </li>
             <li>
               Possibility to provide a marketing poster/video (10s) to show
               during session breaks
             </li>
           </ul>
-          <h1 className={styles.price}> 1000$</h1>
         </div>
         <div className={styles.pack} style={{ backgroundColor: "#fcf8d5" }}>
           <h1> Gold</h1>
@@ -43,18 +45,19 @@ export const SponsorsPacks = () => {
               Special Thanks & welcome to the Gold sponsor in our social medias
             </li>
           </ul>
-          <h1 className={styles.price}> 500$</h1>
         </div>
         <div className={styles.pack} style={{ backgroundColor: "#d6d1fc" }}>
           <h1> Silver</h1>
           <ul>
             <li>
-              Welcome to the silver sponsor post across social media platforms
+              Identification as Silver sponsor in all marketing digital support
+              of the event
             </li>
-            <li>Logo on event promotional materials</li>
-            <li>Special Thanks post across social media platforms</li>
+            <li>
+              Special Thanks & welcome to the Silver sponsor in our social
+              medias
+            </li>
           </ul>
-          <h1 className={styles.price}> 350$</h1>
         </div>
       </div>
       <SponsorButton />
