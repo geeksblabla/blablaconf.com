91b7d5e5b81ff3921afd9d825a605ac81c2a5cbe
Abderrahim SOUBAI-ELIDRISI <gx.coder@gmail.com>
2022-12-04 19:52:45 +0100

    fix: responsive text center

--- components/sponsors.tsx
+++ components/sponsors.tsx
@@ -6,14 +6,14 @@ export const Sponsors = () => {
       <div className="mx-auto max-w-screen-lg px-8 pt-20 pb-16 text-gray-700 md:pt-24 md:pb-20">
         <div className="flex flex-wrap">
           <div className="w-full max-w-full flex-shrink-0 lg:mt-2 lg:w-1/3 lg:flex-none">
-            <h2 className="mb-4 text-3xl font-bold md:mb-6 md:text-4xl capitalize ">
+            <h2 className="mb-4 text-3xl font-bold md:mb-6 md:text-4xl capitalize sm:text-center text-left">
               Our Sponsors
             </h2>
-            <p className=" text-lg font-medium text-gray-600 leading-normal max-w-[350px]  ">
+            <p className=" text-lg font-medium text-gray-600 leading-normal lg:max-w-[350px] sm:text-center text-left">
               We are grateful to our sponsors for their support and contribution
             </p>
           </div>
-          <div className="w-full  max-w-full pt-10 lg:w-2/3 lg:flex-none lg:px-8 lg:py-0 ">
+          <div className="w-full  max-w-full pt-10 lg:w-2/3 lg:flex-none lg:px-8 lg:py-0">
             <div className="h-full flex flex-row justify-start items-end ">
               <a
                 href="https://obytes.com"
