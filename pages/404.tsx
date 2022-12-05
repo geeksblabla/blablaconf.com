12128936cd59ed75a73742bcbcbf40b268b1804b
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2022-12-05 19:32:11 +0100

    fix 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,22 +1,11 @@
+import { Layout } from "@/components/layout";
 import type { NextPage } from "next";
-import GetTicket from "../components/Buttons/GetTicket";
-import { Layout } from "../components/Layout";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
     <>
       <Layout>
-        <main className={styles.main}>
-          <div>
-            <h1 className={styles.title}> Page Not Found ⚠️</h1>
-          </div>
-          <GetTicket />
-          <div className={styles.ticket_container}>
-            <div className={styles.ticket_placeholder}></div>
-            <img className={styles.ticket} src="/img/ticket.png" alt="Ticket" />
-          </div>
-        </main>
+        <div className="h-full"> 404</div>
       </Layout>
     </>
   );
