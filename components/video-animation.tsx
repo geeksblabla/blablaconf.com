7e71c07558c06fd84083021ee69aab9e34cea131
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2024-01-29 16:16:00 +0100

    feat: gif instead of video animation

--- components/video-animation.tsx
+++ components/video-animation.tsx
@@ -1,31 +1,23 @@
-"use client";
-
-import { useLayoutEffect } from "react";
-
 export const VideoAnimation = () => {
-  useLayoutEffect(() => {
-    const video = document.getElementById(
-      "video-animation"
-    ) as HTMLVideoElement;
-    if (video) {
-      video.play();
-    }
-  }, []);
   return (
-    <video
-      id="video-animation"
-      autoPlay
-      loop
-      muted
-      playsInline
-      controls={false}
-      className="md:mx-auto -ml-16 pointer-events-none max-w-[1000px] min-w-[600px] w-full self-end"
-    >
-      <source
-        src="/images/people-walking-2.mov"
-        type='video/mp4; codecs="hvc1"'
-      />
-      <source src="/images/people-walking.webm" type="video/webm" />
-    </video>
+    <>
+      <picture>
+        <source
+          media="(max-width: 499px)"
+          srcSet="https://res.cloudinary.com/duko2tssr/image/upload/c_scale,q_100,w_700/v1706540494/People_walking_optimized_1_wp73wg.gif"
+          className="md:mx-auto -ml-16 pointer-events-none max-w-[1000px] min-w-[600px] w-full self-end"
+        />
+        <source
+          media="(min-width: 500px)"
+          srcSet="https://res.cloudinary.com/duko2tssr/image/upload/c_scale,q_100,w_1000/v1706540494/People_walking_optimized_1_wp73wg.gif"
+          className="md:mx-auto -ml-16 pointer-events-none max-w-[1000px] min-w-[600px] w-full self-end"
+        />
+        <img
+          srcSet="https://res.cloudinary.com/duko2tssr/image/upload/c_scale,q_100,w_700/v1706540494/People_walking_optimized_1_wp73wg.gif"
+          alt="people walking to the conference"
+          className="md:mx-auto -ml-16 pointer-events-none max-w-[1000px] min-w-[600px] w-full self-end"
+        />
+      </picture>
+    </>
   );
 };
